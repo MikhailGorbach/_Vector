e8c6a51a958ede8278c55c195a15facb1d1681c1
Bacha <86574214+MikhailGorbach@users.noreply.github.com>
2021-09-29 21:29:40 +0300

    Revert "Revert "Dev""

---
+++ app/src/main/java/com/example/vector/screens/RegisterFragment.kt
@@ -0,0 +1,19 @@
+package com.example.vector.screens
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.vector.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register)
+{
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.reg_rb_log).setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+    }
+}
