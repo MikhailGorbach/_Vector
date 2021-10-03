d63b8b9ab96b7ffc7fa42d22799f2f4c19853f69
MikhailGorbach <michagorbach57@gmail.com>
2021-10-03 21:41:38 +0300

    refactor

--- app/src/main/java/com/example/vector/screens/RegisterFragment.kt
+++ app/src/main/java/com/example/vector/screens/RegisterFragment.kt
@@ -1,25 +1,23 @@
 package com.example.vector.screens
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
-import android.widget.Switch
-import android.widget.Toast
-import androidx.appcompat.widget.SwitchCompat
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.vector.R
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterFragment: Fragment(R.layout.fragment_register)
 {
-    val pwd = view?.findViewById<TextInputEditText>(R.id.reg_tiet_password)
-    val pwd_again = view?.findViewById<TextInputEditText>(R.id.reg_tiet_pass_again)
-    val switch = view?.findViewById<SwitchCompat>(R.id.reg_sw_agree)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-            view.findViewById<Button>(R.id.reg_bt_enter).setOnClickListener {
+            view.findViewById<Button>(R.id.btn_new_user).setOnClickListener {
                 if (validationIsOk()) {
                     findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
                 }
