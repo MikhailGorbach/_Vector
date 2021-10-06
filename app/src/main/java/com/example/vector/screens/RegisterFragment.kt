18d1513be74bad35d63005a25c744660c26fb21f
MikhailGorbach <michagorbach57@gmail.com>
2021-10-06 23:32:47 +0300

    ready for pr

--- app/src/main/java/com/example/vector/screens/RegisterFragment.kt
+++ app/src/main/java/com/example/vector/screens/RegisterFragment.kt
@@ -11,59 +11,11 @@ import com.example.vector.R
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
-class RegisterFragment: Fragment(R.layout.fragment_register)
-{
-    var login = view?.findViewById<TextInputEditText>(R.id.loginEt)
-    var pwd = view?.findViewById<TextInputEditText>(R.id.pwdSecondEt)
-    var email = view?.findViewById<TextInputEditText>(R.id.emailEt)
-    var pwdAgain = view?.findViewById<TextInputEditText>(R.id.pwdSecondEt)
-
-    var loginTl = view?.findViewById<TextInputLayout>(R.id.loginTl)
-    var emailTl = view?.findViewById<TextInputLayout>(R.id.emailTl)
-    var pwdTl = view?.findViewById<TextInputLayout>(R.id.pwdFirstTl)
-    var pwdAgainTl = view?.findViewById<TextInputLayout>(R.id.pwdSecondTl)
-
-    lateinit var binding : RegisterFragment
+class RegisterFragment: Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-            view.findViewById<Button>(R.id.registrationBtn).setOnClickListener {
-                findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
-            }
-    }
-    inner class TextValidation(private val view: View) : TextWatcher {
-
-        override fun afterTextChanged(p0: Editable?) {
-        }
-
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        }
-
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-        }
-        private fun validateUser(): Boolean {
-            if (binding.login?.text.toString().trim().isEmpty()) {
-                binding.loginTl?.error = "Обязательное поле"
-                binding.login?.requestFocus()
-                return false
-            }
-            binding.loginTl?.isErrorEnabled = false
-            return true
-        }
-        private fun validateEmail(): Boolean {
-            if (binding.email?.text.toString().trim().isEmpty()) {
-                binding.emailTl?.error = "Обязательное поле"
-                binding.email?.requestFocus()
-                return false
-            }
-            //else if (!isValid())
-            return true
-        }
-        private fun isValid(): Boolean {
-            if (validateEmail() && validateUser())
-            return true
-            return false
+        view.findViewById<Button>(R.id.registrationBtn).setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
     }
 }
-
