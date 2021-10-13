b695b7394c966075836111a194d25d42acdc67a2
Bacha <michagorbach57@gmail.com>
2021-10-13 23:50:55 +0300

    added helper text, new validation, view binding

--- app/src/main/java/com/example/vector/screens/RegisterFragment.kt
+++ app/src/main/java/com/example/vector/screens/RegisterFragment.kt
@@ -5,53 +5,98 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.vector.R
 import com.example.vector.data.User
 import com.example.vector.data.UserViewModel
+import com.example.vector.databinding.FragmentRegisterBinding
 import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
     private lateinit var mUserViewModel: UserViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
-        view.findViewById<AppCompatButton>(R.id.registrationBtn).setOnClickListener {
+        binding.registrationBtn.setOnClickListener {
             insertDataToDatabase()
         }
-        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     private fun insertDataToDatabase() {
-        val login = loginEt.text.toString()
-        val email = emailEt.text.toString()
-        val password = pwdFirstEt.text.toString()
-        val passwordAgain = pwdSecondEt.text.toString()
-        val agree = agreeSwtch.isChecked
-        if (inputCheck(login, email, password, passwordAgain) && agree) {
-            val user = User(0, login, email, password)
+        if (inputCheck()) {
+            val user = User(
+                0,
+                binding.loginEt.text.toString(),
+                binding.emailEt.text.toString(),
+                binding.pwdFirstEt.text.toString()
+            )
             mUserViewModel.addUser(user)
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
     }
 
-    private fun inputCheck(
-        login: String,
-        email: String,
-        password: String,
-        passwordAgain: String
-    ): Boolean {
-        return !(TextUtils.isEmpty(login) || TextUtils.isEmpty(email)
-                || TextUtils.isEmpty(password) || passwordAgain != password
-                || !email.contains("@") || password.length < 5
-                || login.length < 5 || email.length < 5)
+    private fun inputCheck(): Boolean {
+        return (loginCheck() && emailCheck() && passwordCheck() && passwordMatch() && binding.agreeSwtch.isChecked)
+    }
+
+    private fun passwordMatch(): Boolean {
+        if (pwdFirstEt.text.toString() != pwdSecondEt.text.toString()) {
+            binding.pwdFirstTl.helperText = "Пароли не совпадают"
+            binding.pwdSecondTl.helperText = "Пароли не совпадают"
+            return false
+        }
+        binding.pwdFirstTl.helperText = ""
+        binding.pwdSecondTl.helperText = ""
+        return true
+    }
+
+    private fun loginCheck(): Boolean {
+        val login = binding.loginEt.text.toString()
+        binding.pwdFirstTl.onFocusChangeListener
+        if (login.length < 5) {
+            binding.loginTl.helperText = "Меньше 5 символов"
+            return false
+        }
+        binding.loginTl.helperText = ""
+        return true
+    }
+
+    private fun emailCheck(): Boolean {
+        val email = binding.emailEt.text.toString()
+        if (email.length < 7 || !email.contains("@") || !(email.contains(".com") || email.contains(".ru"))) {
+            binding.emailTl.helperText = "Неправильный адрес"
+            return false
+        }
+        binding.emailTl.helperText = ""
+        return true
+    }
+
+    private fun passwordCheck(): Boolean {
+        val password = binding.pwdFirstEt.text.toString()
+        if (password.length < 6) {
+            binding.pwdFirstTl.helperText = "Меньше 6 символов"
+            return false
+        }
+        binding.pwdFirstTl.helperText = ""
+        return true
     }
 }
+
