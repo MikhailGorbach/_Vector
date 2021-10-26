ac011261f4fde63795514d19131762f836a58ce4
Bacha <michagorbach57@gmail.com>
2021-10-26 22:40:52 +0300

    searchview, markers on places

--- app/src/main/java/com/example/vector/ui/MapFragment.kt
+++ app/src/main/java/com/example/vector/ui/MapFragment.kt
@@ -2,38 +2,60 @@ package com.example.vector.ui
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.location.Location
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.vector.databinding.FragmentMapBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentMapBinding
     private lateinit var mMap: GoogleMap
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var lastLocation: Location
-    private var marksList: List<String>? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentMapBinding.inflate(inflater, container, false)
         binding.mapView.onCreate(savedInstanceState)
         binding.mapView.onResume()
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val location = binding.searchView.query.toString()
+                if (location != "") {
+                    val geocoder = Geocoder(requireContext())
+                    try {
+                        var addressList: List<Address> = geocoder.getFromLocationName(location, 1)
+                        while (addressList.isEmpty()) {
+                            addressList = geocoder.getFromLocationName(location, 1)
+                        }
+                        if (addressList.isNotEmpty()) {
+                            val address = addressList[0]
+                            val latLng = LatLng(address.latitude, address.longitude)
+                            placeMarker(latLng)
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12f))
+                        }
+                    } catch (e: Exception) {
+                        println(e)
+                    }
+                }
+                return false
+            }
+        })
         binding.mapView.getMapAsync(this)
-        fusedLocationClient =
-            LocationServices.getFusedLocationProviderClient(requireActivity())
         return binding.root
     }
 
@@ -42,31 +64,14 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
             mMap = map
         }
         map?.uiSettings?.isZoomControlsEnabled = true
-        mMap.setOnMarkerClickListener(this)
         setUpMap()
     }
 
     private fun setUpMap() {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
         ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                1
-            )
-            return
-        }
-        mMap.isMyLocationEnabled = true
-        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
-            if (location != null) {
-                lastLocation = location
-                val currentLatLong = LatLng(location.latitude, location.longitude)
-                placeMarker(currentLatLong)
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
-            }
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
         }
     }
 
@@ -75,6 +80,4 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
         markerOptions.title("$currentLatLong")
         mMap.addMarker(markerOptions)
     }
-
-    override fun onMarkerClick(p0: Marker) = false
 }
