85c91f60f4296452c7146a4c914d69376d7f4735
Bacha <michagorbach57@gmail.com>
2021-11-01 22:33:25 +0300

    fixed bug

--- app/src/main/java/com/example/vector/domain/local/DataBase.kt
+++ app/src/main/java/com/example/vector/domain/local/DataBase.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.vector.domain.local.entity.MarkDto
 import com.example.vector.domain.local.entity.UserDto
 
-@Database(entities = [UserDto::class], version = 1, exportSchema = false)
+@Database(entities = [UserDto::class, MarkDto::class], version = 1, exportSchema = false)
 abstract class DataBase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
